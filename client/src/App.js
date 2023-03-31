4ace1b1eb0722af66aca6d03bcd8d38fb77146e9
vishswati1011 <sati.v1011@gmail.com>
2023-03-31 22:39:47 +0530

    kanban code with socket

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Comments from "../src/component/Comment";
 import Task from "./component/Task/Task";
-import ListKanban from "./component/Listview/ListKanban";
 import Login from "../src/component/Login";
-import Listview from "../src/component/Listview/Listview"
-import View from "../src/component/Listview/view"
-import Kanban from '../src/component/kanban/Task'
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Login />} />
-                <Route path='/dnd' element={<Listview/>}/>
-                <Route path='/list' element={<ListKanban/>} />
-                <Route path='/view' element={<View />} />
-               
                 <Route path='/tasks' element={<Task />} />
-                <Route path='/kanban' element={<Kanban />} />
-
-
                 <Route path='/comments/:category/:id' element={<Comments />} />
 
             </Routes>
